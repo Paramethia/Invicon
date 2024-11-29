9c3176c5220fdd40b27fe949f70806442d80877a
Kyrin <143742919+Paramethia@users.noreply.github.com>
2024-11-29 23:12:48 +0200

    Update configuratoin

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'dist',
+    rollupOptions: {
+      input: '/index.html',
+    },
   },
-  plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
+  plugins: [ react() ],
 })
