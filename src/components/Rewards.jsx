bc62dcd95f70d13c1a66fe3eeaf4593671e28241
Kyrin <143742919+Paramethia@users.noreply.github.com>
2025-02-27 21:19:49 +0200

    Remove rewards

--- src/components/Rewards.jsx
+++ src/components/Rewards.jsx
@@ -290,7 +290,7 @@ const Rewards = () => {
                         <a
                           className="text-center bg-[#282434] text-white font-bold py-2 px-4 rounded transition-colors hover:bg-[#3c3a4e]"
                         >
-                            <button id="reward-button" className="bg-gray-500 hover:bg-blue-500 dark:bg-gray-700 text-gray-900 dark:text-white px-4 py-2 rounded-md"}>
+                            <button id="reward-button" className="bg-gray-500 hover:bg-blue-500 dark:bg-gray-700 text-gray-900 dark:text-white px-4 py-2 rounded-md">
                                 Claim reward
                             </button>
                         </a>
