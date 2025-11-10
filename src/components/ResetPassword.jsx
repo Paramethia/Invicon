4d5d562f5352ea670eb08b6afa328d661f0270d2
Kyrin <kyrinkompi@gmail.com>
2025-11-10 16:13:58 +0200

    Fixed and improved some tings

--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { useNavigate, useLocation } from 'react-router-dom';
 import axios from 'axios';
@@ -12,7 +12,6 @@ const Header = () => {
         </Helmet>
     );
 };
-
 
 const ResetPassword = () => {
     const [password, setPassword] = useState('');
@@ -29,7 +28,7 @@ const ResetPassword = () => {
             return;
         }
         try {
-            const response = await axios.post("https://invicon-back-end.onrender.com/reset-password", { token, newPassword: password });
+            const response = await axios.post("https://invicon-server-x4ff.onrender.com/reset-password", { token, newPassword: password });
             toast.info(response.data, {
                 position: "top-right",
                 autoClose: 4000,
@@ -49,45 +48,37 @@ const ResetPassword = () => {
 
     return (
         <>
- 
         <Header />
-
         <ToastContainer />
-
         <div className="flex  h-screen">
             <div className="hidden md:block md:w-1/2 bg-auto" style={{ backgroundImage: "url(https://res.cloudinary.com/dw7w2at8k/image/upload/v1720626946/Home_1_d6rirw.png)" }}></div>
-            <div className="w-full md:w-1/2 flex flex-col items-center justify-center bg-gray-100">
+            <div className="w-full md:w-1/2 flex flex-col items-center justify-center bg-gray-400">
                 <h1 className="block md:hidden mb-6 text-4xl font-bold text-dark">Invicon</h1>
-                <div className="bg-white p-8 rounded shadow-md w-3/4 animate__animated animate__fadeInRight">
-        <h3 className="mb-6 text-2xl text-center font-bold text-dark">Change Password</h3>
-
-        <form onSubmit={handleSubmit}>
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-            <div className="mb-4 text-left"> <input
-                type="password"
-                placeholder="Enter new password"
-                className="form-control block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            /></div>
-            <div className="mb-4 text-left">
-            <input
-                type="password"
-                placeholder="Confirm new password"
-                className="form-control block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-            />
+                <div className="bg-300 p-8 rounded shadow-md w-3/4 animate__animated animate__fadeInRight">
+                    <h3 className="mb-6 text-2xl text-center font-bold text-dark">Change Password</h3>
+                    <form onSubmit={handleSubmit}>
+                        {error && <p style={{ color: 'red' }}>{error}</p>}
+                        <div className="mb-4 text-left"> <input
+                            type="password"
+                            placeholder="Enter new password"
+                            className="form-control block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        /></div>
+                        <div className="mb-4 text-left">
+                            <input
+                                type="password"
+                                placeholder="Confirm new password"
+                                className="form-control block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                            />
+                        </div>
+                        <button type="submit" className="w-full bg-dark text-white py-2 rounded-md hover:bg-dark transition duration-300 ease-in-out transform hover:scale-105">Reset Password</button>
+                    </form>
+                </div>
             </div>
-            
-            <button type="submit" className="w-full bg-dark text-white py-2 rounded-md hover:bg-dark transition duration-300 ease-in-out transform hover:scale-105">Reset Password</button>
-        </form>
         </div>
-        </div>
-        </div>
-
         </>
     );
 };
