7c23f378069074094a7af2563e692f25789eaf03
Kyrin <kyrinkompi@gmail.com>
2025-11-10 16:25:02 +0200

    Fixed and improved some tings

--- src/components/ResetPassword.jsx
+++ src/components/ResetPassword.jsx
@@ -58,17 +58,22 @@ const ResetPassword = () => {
                     <h3 className="mb-6 text-2xl text-center font-bold text-dark">Change Password</h3>
                     <form onSubmit={handleSubmit}>
                         {error && <p style={{ color: 'red' }}>{error}</p>}
-                        <div className="mb-4 text-left"> <input
-                            type="password"
-                            placeholder="Enter new password"
-                            className="form-control block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        /></div>
+                        <div className="mb-4 text-left"> 
+                            <input
+                                type="password"
+                                placeholder="Enter new password"
+                                minLength="4"
+                                maxLength="17"
+                                className="form-control block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
                         <div className="mb-4 text-left">
                             <input
                                 type="password"
                                 placeholder="Enter it again"
+                                minLength="4"
                                 maxLength="17"
                                 className="form-control block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                                 value={confirmPassword}
